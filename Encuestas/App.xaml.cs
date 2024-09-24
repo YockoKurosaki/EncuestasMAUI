b11f7e90fc7eea3cef282df99b402794a15b8f24
María Torres <mtorres@mrhouston.net>
2024-09-24 12:35:18 +0200

    init

---
+++ Encuestas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Encuestas
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ Encuestas/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Encuestas
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
